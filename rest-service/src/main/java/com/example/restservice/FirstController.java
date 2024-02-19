f40c0627a67b52f12472eb20063b0cd61f26886b
sahamahmed <sahamahmed70@gmail.com>
2024-02-19 14:14:11 +0500

    commit

--- rest-service/src/main/java/com/example/restservice/FirstController.java
+++ rest-service/src/main/java/com/example/restservice/FirstController.java
@@ -1,11 +1,9 @@
 package com.example.restservice;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.Optional;
+
 
 @RestController
 public class FirstController {
--- rest-service/src/main/java/com/example/restservice/StudentService.java
+++ rest-service/src/main/java/com/example/restservice/StudentService.java
@@ -1,8 +1,7 @@
 package com.example.restservice;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+
 
 import java.util.List;
 import java.util.Optional;
