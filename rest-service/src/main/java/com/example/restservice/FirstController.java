c5e40c4fbf4dcba7ae28f0178d0853ddf62ad13a
sahamahmed <sahamahmed70@gmail.com>
2024-02-19 14:15:35 +0500

    commit

--- rest-service/src/main/java/com/example/restservice/FirstController.java
+++ rest-service/src/main/java/com/example/restservice/FirstController.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-
 @RestController
 public class FirstController {
     private final StudentService service;
